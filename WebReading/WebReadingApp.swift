d46cb447bf0ded948599d43c4f4c30a583655cb6
savansamar <savansoni125@gmail.com>
2025-09-05 13:39:16 +0530

    Initial Commit

---
+++ WebReading/WebReadingApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebReadingApp.swift
+//  WebReading
+//
+//  Created by MACM72 on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct WebReadingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
