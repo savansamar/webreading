a71d1625f281356350353738b4f578c923ee1d39
savansamar <savansoni125@gmail.com>
2025-09-08 14:43:26 +0530

    first commit

---
+++ WebReading/Views/ReadingDataEditorView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingDataEditorView.swift
+//  WebReading
+//
+//  Created by MACM72 on 08/09/25.
+//
+
+import SwiftUI
+
+struct ReadingDataEditorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadingDataEditorView()
+}
---
+++ WebReading/Views/ReadingListView.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingListView.swift
+//  WebReading
+//
+//  Created by MACM72 on 05/09/25.
+//
+
+import SwiftUI
+
+struct ReadingListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadingListView()
+}
